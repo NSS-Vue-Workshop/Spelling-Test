b18e5965d6e3b1fbd05db7b23e08976138c4fbe8
Adam <adam.e.sheaffer@gmail.com>
2021-01-10 17:51:23 -0600

    spelling test walkthrough

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Spelling Test</h1>
+    <div v-if="!testFinished">
+      <p>Word {{ activeIndex + 1 }} of {{ questions.length }}</p>
+      <speech :word="questions[activeIndex].word" />
+      <form @submit.prevent="handleSubmit">
+        <input
+          type="text"
+          v-model="userInput"
+          spellcheck="false"
+          placeholder="Spell the word"
+        />
+        <button type="submit">SUBMIT</button>
+      </form>
+    </div>
+    <div v-else>
+      <score :data="questions" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import data from "./data";
+import Speech from "./components/Speech";
+import Score from "./components/Score";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  components: { Speech, Score },
+
+  data() {
+    return {
+      questions: data,
+      activeIndex: 0,
+      userInput: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.questions[this.activeIndex].userInput = this.userInput;
+      this.activeIndex += 1;
+      this.userInput = "";
+    }
+  },
+  computed: {
+    testFinished() {
+      return this.questions.every(q => q.userInput);
+    }
   }
 };
 </script>
@@ -24,5 +58,36 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  color: #333;
+  font-size: 2.5rem;
+  margin: 0 auto;
+  padding: 1.5rem 2rem;
+  border: 4px solid black;
+  border-radius: 0.2rem;
+  width: 40%;
+  display: block;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+button {
+  display: inline-block;
+  padding: 0.6em 1.2em;
+  border: 0.1em solid black;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.2rem;
+  box-sizing: border-box;
+  text-decoration: none;
+  text-align: center;
+  transition: all 0.4 s;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+button:hover {
+  color: white;
+  background-color: black;
 }
 </style>
---
+++ src/components/Speech.vue
@@ -0,0 +1,34 @@
+<template>
+  <div @click="sayWord" class="clickable">
+    <img src="/speak.jpg" alt="Speak" />
+    <h3>Say Word</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["word"],
+  methods: {
+    sayWord() {
+      const utterance = new SpeechSynthesisUtterance(this.word);
+      speechSynthesis.speak(utterance);
+    }
+  },
+
+  // Optional. See Vue watchers
+  watch: {
+    word() {
+      this.sayWord();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.clickable {
+  cursor: pointer;
+}
+h3 {
+  margin-top: -35px;
+}
+</style>
